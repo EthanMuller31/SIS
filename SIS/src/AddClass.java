50e1485080c491117a64c96f301a545eac59d84d
genevieve.hockin <genevieve.hockin@MU-C1-04.mullen.pvt.k12.co.us>
2024-03-19 13:22:33 -0600

    fixing

--- SIS/src/AddClass.java
+++ SIS/src/AddClass.java
@@ -4,40 +4,43 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class AddClass {
-	
+
 	public static void addStudent() throws IOException
 		{
 		StudentList.fileRead();
-		Scanner studentFile = new Scanner(new File("studentData.txt"));
-		
-		Scanner scanner = new Scanner(System.in);
+		Scanner studentFile = new Scanner(new File("studentData.txt"));		
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("Enter the student details");
 		//firstname
 		System.out.print("First Name");
-		String firstName = scanner.next();
+		String firstName = input.next();
 		//lastname
 		System.out.print("Last  Name");
-		String lastName = scanner.next();
+		String lastName = input.next();
+		//gpa
+		System.out.print("GPA");
+		double gpa = input.nextDouble();
 		//alg grade
 		System.out.print("Algebra Grade");
-		int algGrade = scanner.nextInt();
+		int algGrade = input.nextInt();
 		//eng grade
 		System.out.print("English Grade");
-		int engGrade = scanner.nextInt();
+		int engGrade = input.nextInt();
 		//chem grade
 		System.out.print("Chemistry Grade");
-		int chemGrade = scanner.nextInt();
+		int chemGrade = input.nextInt();
 
 		//adding the new student 
 		Student newStudent = new Student(firstName, lastName, gpa, algGrade, engGrade, chemGrade);
-		StudentList.students.add(newStudent);
+
 		System.out.print("added");
 			}
 		public static  void deleteStudent()
 		{ 
-			Scanner scanner = new Scanner(System.in);
+			Scanner input = new Scanner(System.in);
 			System.out.println("Enter the student you want to delete");
-			int index = scanner.nextInt();
+			int index = input.nextInt();
 
 			if(index >=0 && index <StudentList.students.size())
 			{
